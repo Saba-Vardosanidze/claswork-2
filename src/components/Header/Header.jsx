2b07824d9895b099b5fdbef632aedd7e30ad743d
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-03-20 21:23:08 +0400

    update/logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <p className={styles.logo}>Startup 3</p>
+      <a href="#" className={styles.logo}>
+        Startup 3
+      </a>
       <Navigation />
     </header>
   );
